e7af18e0b1006af5b34898d23a8f05db8de319bd
tramNhuynh <70813686+tramNhuynh@users.noreply.github.com>
2020-10-01 22:09:29 -0700

    Create Madlibs.rb

---
+++ Madlibs.rb
@@ -0,0 +1,26 @@
+puts "Welcome to my Madlib program, please enter a response for these selection."
+
+print "noun: "
+noun_1 = gets.chomp
+print "state name: "
+state_name = gets.chomp
+state_name.capitalize
+print "noun: "
+noun_2 = gets.chomp
+number = gets.chomp
+print "adjective: "
+adjective_1 = gets.chomp
+print "noun: "
+noun_3 = gets.chomp
+print "verb: "
+verb_1 = gets.chomp
+print "noun: "
+noun_4 = gets.chomp
+print "verb: "
+verb_2 = gets.chomp
+print "adverb (ending in -ly): "
+adverb_1 = gets.chomp
+
+
+puts"There once was a(n) #{noun_1} from #{state_name.capitalize} who knew he would become an amazing #{noun_2} if only he could inhale #{number}  #{adjective_1} #{noun_3}. He made an attempt to #{verb_1} a(n) #{noun_4} but it was 
+not meant to be. All he could do was #{verb_2} #{adverb_1}. Then before he knew it his dream of becoming #{noun_2} came true." 
